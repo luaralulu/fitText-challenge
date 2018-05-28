bed6a222445c4f7397d225b0ed8b66b1e3b24b53
Luara Neumann <luaralulu@gmail.com>
2018-05-28 11:42:18 +0200

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+// Resize the DIV
+var slider = document.getElementById("myRange");
+var output = document.getElementById("output");
+var fit = document.getElementById("fit");
+
+slider.oninput = function() {
+    output.style.width = this.value + "px";
+    fitty("#fit");
+};
+
+// Change DIV text
+document.getElementById("txt").addEventListener("input", function(evt) {
+    fit.innerHTML = this.value;
+});
