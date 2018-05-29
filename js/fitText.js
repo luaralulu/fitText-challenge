260261de3b71ad1bfefc489f6d51f8493cbac565
Luara Neumann <luaralulu@gmail.com>
2018-05-29 11:21:31 +0200

    refactoring fitText so other functions are not exposed

--- js/fitText.js
+++ js/fitText.js
@@ -1,36 +1,41 @@
 var fitText = function(e) {
   $(e).css("white-space", "nowrap");
-  var fontSize = parseFloat($(e).css("font-size"));
 
-  if (shouldReduce(e)) {
-    reduce(e, fontSize);
-  } else if (shouldIncrease(e)) {
-    increase(e, fontSize);
-  }
-};
+  var resize = function(e) {
+    var fontSize = parseFloat($(e).css("font-size"));
 
-var reduce = function(e, fontSize) {
-  $(e).css("font-size", fontSize);
-  if (shouldReduce(e)) {
-    fontSize -= 2;
-    reduce(e, fontSize);
-  }
-};
+    var shouldReduce = function(e) {
+      return $(e).height() > $("#output").height() || $(e).width() > $("#output").width() ? true : false;
+    };
 
-var increase = function(e, fontSize) {
-  $(e).css("font-size", fontSize);
-  if (shouldIncrease(e)) {
-    fontSize += 1;
-    increase(e, fontSize);
-  }
-};
+    var shouldIncrease = function(e) {
+      return $(e).height() < $("#output").height() && $(e).width() < $("#output").width() ? true : false;
+    };
 
-var shouldReduce = function(e) {
-  return $(e).height() > $("#output").height() || $(e).width() > $("#output").width() ? true : false;
-};
+    var reduce = function(e, fontSize) {
+      $(e).css("font-size", fontSize);
+      if (shouldReduce(e)) {
+        fontSize -= 2;
+        reduce(e, fontSize);
+      }
+    };
 
-var shouldIncrease = function(e) {
-  return $(e).height() < $("#output").height() && $(e).width() < $("#output").width() ? true : false;
+    var increase = function(e, fontSize) {
+      $(e).css("font-size", fontSize);
+      if (shouldIncrease(e)) {
+        fontSize += 1;
+        increase(e, fontSize);
+      }
+    };
+
+    if (shouldReduce(e)) {
+      reduce(e, fontSize);
+    } else if (shouldIncrease(e)) {
+      increase(e, fontSize);
+    }
+  };
+
+  resize(e);
 };
 
 // Scale the DIV
